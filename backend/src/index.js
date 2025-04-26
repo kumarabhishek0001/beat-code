b0c992f3a3a42039e816e82c3529da1fe1ae2106
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-04-26 18:49:00 +0530

    feat: boilerplate code ready for index.js
    prisma is initialized -> schema.prisma generated

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const port = process.env.PORT
+const app = express()
+
+app.get('/',(req,res) => {
+    res.send('Welcome to leetlab🔥🛠️')
+})
+
+app.listen(port, ()=>{
+    console.log('server listening to port:', port)
+})
---
+++ nodes.md
@@ -0,0 +1,4 @@
+## Procedure
+1. Basic setup for express file
+2. npm i prisma -> npm i @prisma/client -> npx prisma init
+
