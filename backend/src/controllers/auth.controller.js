7ff6c94ecb738e5e8b4cce78f6dd903e5f3e5b9d
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-05-04 00:11:49 +0530

    feat: add controllers for loin logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,11 +67,95 @@ export const register = async(req, res) => {
 }
 
 export const login = async(req, res) => {
+    //
+    const {email, password} = req.body
 
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(!user){
+            return res.status(401).json({
+                error: "user does not exist"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch){
+            return res.status.json({
+                error: 'Invalid Credentials'
+            })
+        }
+
+        const token = jwt.sign({id:user.id},process.env.JWT_SECRET,
+            {expiresIn:"7d"}
+        )
+
+        res.cookie("jwt", token, {
+            httponly : true,
+            sameside : "strict",
+            secure : process.env.NODE_ENV !== "development",
+            maxAge : 1000 * 60 * 60 * 24 * 7 // max age = 7days 
+    
+        })
+
+        res.status(201).json({
+            message : 'user logged In successfully',
+            user : {
+                id : user.id,
+                email : user.email,
+                name : user.role,
+                role : user.role,
+                jwt: token
+            }
+        })
+
+    } catch (error) {
+        console.error('error registering the user in auth.controller.js', error)
+            res.status(500).json({
+                success : false,
+                message : 'something went wrong in auth.controller in login',
+                error
+            })
+    }
 }
 export const logout = async(req, res) => {
+    try {
+        res.clearCookie("jwt",{
+            httponly : true,
+            sameside : "strict",
+            secure : process.env.NODE_ENV !== "development",
+            maxAge : 1000 * 60 * 60 * 24 * 7 // max age = 7days 
+        })
 
+        res.status(201).json({
+            message: 'Logged Out successfully',
+            success: true
+        })
+    } catch (error) {
+        console.error('error logging out the user in auth.controller.js', error)
+            res.status(500).json({
+                success : false,
+                message : 'something went wrong in auth.controller in login',
+                error
+            })
+    }
 }
 export const check = async(req, res) => {
-
+    try {
+        res.status(200).json({
+            success: true,
+            message: 'user authenticated successfully'
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: "error occured while authentication",
+            error
+        })
+    }
 }
